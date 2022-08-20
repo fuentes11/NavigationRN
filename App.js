1939343a2319fd9ea2a372750af7077d27443c87
Diego Sandoval <sandovaldiego21@gmail.com>
2022-08-20 00:07:55 -0600

    Final

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
+
 import { StyleSheet, Text, View } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import 'react-native-gesture-handler';
 import home from './Screens/home';
 import menu from './Screens/menu';
 import about from './Screens/about';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+
 
 const Menu = createDrawerNavigator()
+const Stack = createBottomTabNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
@@ -14,11 +23,39 @@ export default function App() {
 <Menu.Screen name="Home" component={home} />
 <Menu.Screen name="About" component={about} />
 <Menu.Screen name="Menu" component={menu} />
+<Menu.Screen name="Tab" component={Tab} />
 </Menu.Navigator>
 </NavigationContainer>
+
+   
   );
 }
 
+function Tab(){
+  return(
+
+
+<Stack.Navigator>
+<Stack.Screen name="Home" component={home} />
+<Stack.Screen name="About" component={about} />
+<Stack.Screen name="Menu" component={menu} />
+
+</Stack.Navigator>
+
+
+  );
+};
+function Men(){
+  return(
+
+<Menu.Navigator>
+<Menu.Screen name="Home" component={home} />
+<Menu.Screen name="About" component={about} />
+<Menu.Screen name="Menu" component={menu} />
+</Menu.Navigator>
+
+  )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -27,4 +64,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
